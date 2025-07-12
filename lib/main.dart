d5b52fd47d2bac937ebcefc1eba6402967064d18
So2ln <sorrinj77@snu.ac.kr>
2025-07-13 01:26:49 +0900

    Enhance MyHomePage UI and functionality with counter display and increment logic

--- lib/main.dart
+++ lib/main.dart
@@ -25,11 +25,17 @@ class MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    print('build called');
     return Scaffold(
       appBar: AppBar(title: const Text('Test app')),
-      body: Center(child: Text('$counter')),
+      body: Center(child: Text('$counter', style: TextStyle(fontSize: 100))),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          setState(() {
+            counter++;
+          });
+          print('카운터 증가됨 : $counter');
+        },
         tooltip: 'Increment',
         child: Icon(Icons.add),
       ),
